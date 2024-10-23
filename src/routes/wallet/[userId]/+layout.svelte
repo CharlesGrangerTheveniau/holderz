f232276243e6ae457e5eb0c3a4c653192019fdac
Charles Theveniau <charlestheveniau@Charles-MBA.local>
2024-10-23 08:18:23 +0200

    FE: better overview layout with sidebar at the top instead of left

---
+++ src/routes/wallet/[userId]/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+
+    import Navbar from "$lib/components/Navbar/Navbar.svelte";
+
+</script>
+<div class="content h-full flex flex-col items-center justify-start">
+    <div class="nav-bar bg-surface-400 flex flex-row border-surface-500/30 variant-glass-surface">
+        <Navbar/>
+    </div>
+    <slot/>
+</div>
+
+
+<style>
+    .nav-bar {
+        margin-top: 30px;
+        position: relative;
+        top: 0;
+        border-radius: 5px;
+    }
+</style>
